51ba790c334267b24e6b3d6f33eef76b964fe966
David Watts <davidwatts11@gmail.com>
2022-01-29 17:09:34 +1000

    added get entries, get entries ID, and post entries

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const pool = new Pool({
 const app = express()
 const port = 4000
 
+app.use(express.json())
+
 app.get('/', (req, res) => { 
     res.send( {
         info: "nwtracker API app"
@@ -32,9 +34,28 @@ app.get('/entries', (req, res) => {
     })
 })
 
+app.get('/entries/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    pool.query('SELECT * FROM entries where ID = $1', [id], (error, results) => {
+        if (error) {
+            res.status(422).send({ error: error.message})
+        } else if (results.rows.length == 0) {
+            res.status(404).send( {error: error.message})
+        }else {
+            res.send(results.rows[0])
+        }
+    })
+})
+
 app.post('/entries', (req, res) => {
-    console.log(req.body)
-    res.send(req.body)
+    pool.query('INSERT INTO entries (quantity, total_value, currency, code, name, cat_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *'
+    , [req.body.quantity, req.body.total_value, req.body.currency, req.body.code, req.body.name, req.body.cat_id]), (error, results) => {
+        if (error) {
+            res.status(422).send({ error: error.message})
+        } else {
+            res.send(results.rows[0])
+        } 
+    }
 })
 
 
